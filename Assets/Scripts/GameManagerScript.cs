c22e706512943fbdf7ab05f0d45445754ef2a3e4
Mohamed <elmoufid.mohamed@outlook.com>
2020-03-26 22:12:20 +0100

    Add GameManager

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject gameOverCanvas;
+    public GameObject MainCavas;    
+    public enum GameStates { Play, GameOver};
+    public GameStates gameState = GameStates.Play;
+    private Health heatlhPlayer;
+    void Start()
+    {
+        if (Player == null)
+            Player = GameObject.FindGameObjectWithTag("Player");
+
+
+        heatlhPlayer = Player.GetComponent<Health>();
+
+    }
+
+
+    void Update()
+    {
+        switch(gameState)
+        {
+            case GameStates.Play:
+                if(!heatlhPlayer.isAlive)
+                {
+                    gameState = GameStates.GameOver;                   
+                }
+                break;
+
+            case GameStates.GameOver:
+                gameOverCanvas.SetActive(true);
+                break;
+        }
+
+    }
+}

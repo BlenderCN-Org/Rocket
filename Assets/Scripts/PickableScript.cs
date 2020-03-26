e30bd314ee436d56808148fac7a06919d73b8467
Mohamed <elmoufid.mohamed@outlook.com>
2020-03-26 23:41:47 +0100

    Add land on table to load new level

--- Assets/Scripts/PickableScript.cs
+++ Assets/Scripts/PickableScript.cs
@@ -1,13 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PickableScript : MonoBehaviour
 {
+
+    public GameObject ScoreText;
+    public Text text;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (ScoreText == null)
+            ScoreText = GameObject.FindGameObjectWithTag("Score");
+
+        text = ScoreText.GetComponent<Text>();
     }
 
     // Update is called once per frame
@@ -22,5 +31,11 @@ public class PickableScript : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        string prevScore = text.text.ToString();
+        int score = Convert.ToInt32(prevScore);
+        score++;
+        text.text = Convert.ToString( score);
+
     }
 }
---
+++ Assets/Scripts/TableLevelLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TableLevelLoader : MonoBehaviour
+{
+    public string mLevelToLoad;
+    public Scene sceneToLoad;
+    // Start is called before the first frame update
+   
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(mLevelToLoad);
+            Debug.Log("Table Hitted");
+        }
+            
+     
+    }
+}

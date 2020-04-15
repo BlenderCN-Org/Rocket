b6b9a45af310086303fae6e3ae193313c72ace51
Mohamed <elmoufid.mohamed@outlook.com>
2020-04-15 19:52:30 +0200

    Add Movement Script

---
+++ Assets/Scripts/Movement2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Movement2 : MonoBehaviour
+{
+    public float xSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float hori = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = hori * Time.deltaTime * xSpeed;
+        float xNewPos = xOffset + transform.localPosition.x;
+        transform.localPosition = new Vector3(xNewPos, transform.localPosition.y, transform.localPosition.z);
+        print(xOffset);
+        
+    }
+}

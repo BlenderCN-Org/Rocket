fcde802bb8ea87cd1e38222d02793d4646a8d51b
Mohamed <elmoufid.mohamed@outlook.com>
2020-04-16 00:17:43 +0200

    Add Enenmy Prefab

---
+++ Assets/Scripts/PlayerFollower.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollower : MonoBehaviour
+{
+    public Transform mTarget;
+    public float mSpeed = 30f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(mTarget.position);
+        transform.Translate(0.0f, 0.0f, mSpeed * Time.deltaTime);
+    }
+}
